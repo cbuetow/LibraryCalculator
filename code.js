2fa0fb2db9205cb97434486e22c0a157c9ed964c
buetowcj <cbuetow@my.wctc.edu>
2021-01-27 15:12:33 -0600

    update var to let

--- code.js
+++ code.js
@@ -5,34 +5,34 @@ $(document).ready(
         function calculateFees()
         {
             // Get amount of books turned in
-            var numBooks = $("#numBooks").val();
+            let numBooks = $("#numBooks").val();
             numBooks = parseInt(numBooks);
 
             // Get how many days past due
-            var lateDayBooks = $("#lateDayBooks").val();
+            let lateDayBooks = $("#lateDayBooks").val();
             lateDayBooks = parseInt(lateDayBooks);
 
             // Get amount of DVDs turned in
-            var numDVDs = $("#numDVDs").val();
+            let numDVDs = $("#numDVDs").val();
             numDVDs = parseInt(numDVDs);
 
-            var lateDayDVDs = $("#lateDayDVDs").val();
+            let lateDayDVDs = $("#lateDayDVDs").val();
             lateDayDVDs = parseInt(lateDayDVDs);
 
             // Declare variable for lateFeeBooks
-            var lateFeeBooks = 0.25;
+            let lateFeeBooks = 0.25;
 
             // Declare variable for lateFeeDVDs
-            var lateFeeDVDs = 0.5;
+            let lateFeeDVDs = 0.5;
 
             // Calculate lateDayBooks * numBooks * lateFeeBooks = bookFeeTotal
-            var bookFeeTotal = lateDayBooks * numBooks * lateFeeBooks;
+            let bookFeeTotal = lateDayBooks * numBooks * lateFeeBooks;
 
             // Calculate lateDayDVDs * numDVDs * lateFeeDVDs = dvdFeeTotal
-            var dvdFeeTotal = lateDayDVDs * numDVDs * lateFeeDVDs;
+            let dvdFeeTotal = lateDayDVDs * numDVDs * lateFeeDVDs;
 
             // Add bookFeeTotal + dvdFeeTotal to get grandFeeTotal
-            var grandFeeTotal = bookFeeTotal + dvdFeeTotal;
+            let grandFeeTotal = bookFeeTotal + dvdFeeTotal;
 
             $("#costLibOutput").text(grandFeeTotal.toFixed(2));
 
